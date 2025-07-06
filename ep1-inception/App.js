47580028fe17bdd5380e38a2ef0c3c2584af4a23
Akshaya719 <bodduakshaya22@gmail.com>
2025-07-06 21:57:03 +0530

    Test post-commit hook

--- ep1-inception/App.js
+++ ep1-inception/App.js
@@ -13,24 +13,50 @@ import ReactDOM from "react-dom/client";
  * </div>
  * ReactElement(Object) => HTML(Browser Understands)
  */
-const parent = React.createElement("div",{id:"parent"},
-    React.createElement("div",{id:"child"},
-        [React.createElement("h1",{key:"h1"},"I m an h1 tag"),
-        React.createElement("h2",{key:"h2"},"I am an h2 tag")]),
+// const parent = React.createElement("div",{id:"parent"},
+//     React.createElement("div",{id:"child"},
+//         [React.createElement("h1",{key:"h1"},"I m an h1 tag"),
+//         React.createElement("h2",{key:"h2"},"I am an h2 tag")]),
 
-        React.createElement("div",{id:"child2"},
-            [React.createElement("h1",{key:"h3"},"I m an h1 tag"),
-            React.createElement("h2",{key:"h4"},"I am an h2 tag")],
-    ),
-);
-//JSX
+//         React.createElement("div",{id:"child2"},
+//             [React.createElement("h1",{key:"h3"},"I m an h1 tag"),
+//             React.createElement("h2",{key:"h4"},"I am an h2 tag")],
+//     ),
+// );
+// //JSX
 
-const heading = React.createElement("h1",{id: "heading",xyz:"abc"},"Hello world from React!");
-console.log(heading); //object
-const root = ReactDOM.createRoot(document.getElementById("root"));
-//root where all the react code will run
-root.render(parent);
+// const heading = React.createElement("h1",{id: "heading",xyz:"abc"},"Hello world from React!");
+// console.log(heading); //object
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// //root where all the react code will run
+// root.render(parent);
 //basically display
 //putting heading inside root
 //converting this object to a h1 tag
 // replace the things which are already inside the root
+
+
+
+//React.createElement => Object => HTML Element(render)
+// const heading = React.createElement("h1",{id:"heading"},"Namaste React ");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+//JSX HTML XML syntax
+//easier to create react elements
+
+
+//JSX transpiled before it reaches to the js engine  PARCEL -Babel
+//transpile means convert to code which browser or react can understand
+
+//JSX => React.createElement =>ReactElement-JS Object => HTML Element(render)
+const jsxHeading = (<h1 id="heading" className="head" tabIndex ="1">
+    Namaste React using JSX </h1>);
+//not pure js
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHeading);
+
+
+
+
+
